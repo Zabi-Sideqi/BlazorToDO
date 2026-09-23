20f996cfd5bddb88b9176003d3039bf488785798
Zabi Sideqi <zabi.sideqi@gmail.com>
2026-09-23 11:09:36 +0200

    Add project files.

---
+++ BlazorToDO/BlazorToDO.Client/Components/NavBar.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorToDO.Client.Components
+{
+    public partial class NavBar
+    {
+        [Parameter]
+        public string NavTitle { get; set; } = "Default Navbar Title";
+        [Parameter]
+        public RenderFragment? ChildContent {  get; set; }
+    }
+}
