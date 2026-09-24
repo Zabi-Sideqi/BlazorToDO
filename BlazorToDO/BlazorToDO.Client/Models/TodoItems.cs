425de34f18c136b65d04b701f09aaf9effa9e1fb
Zabi Sideqi <zabi.sideqi@gmail.com>
2026-09-24 09:25:39 +0200

    Create Item Component and TodoItem.cs

---
+++ BlazorToDO/BlazorToDO.Client/Models/TodoItems.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorToDO.Client.Models
+{
+    public class TodoItems
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Text { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public bool Completed { get; set; }
+    }
+}
