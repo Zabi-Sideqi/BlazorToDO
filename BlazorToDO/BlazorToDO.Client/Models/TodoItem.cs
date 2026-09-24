ba806c1bb31fa7937c0cc5a51bbd0313af0571e6
Zabi Sideqi <zabi.sideqi@gmail.com>
2026-09-24 10:34:29 +0200

    Move privet arry to items index

--- BlazorToDO/BlazorToDO.Client/Models/TodoItem.cs
+++ BlazorToDO/BlazorToDO.Client/Models/TodoItem.cs
@@ -1,10 +1,10 @@
 ﻿namespace BlazorToDO.Client.Models
 {
-    public class TodoItems
+    public class TodoItem
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public required string Text { get; set; }
-        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime Created { get; set; } = DateTime.Now;
         public bool Completed { get; set; }
     }
 }
